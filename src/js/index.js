bc831c804ea0afe774d8d033d6719ba7188c6952
slv-improver <k.moret.slv@gmail.com>
2022-08-17 23:07:09 +0100

    change categories constants

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,19 @@
 const api = 'http://127.0.0.1:8000/api/v1';
 const movies = api + '/titles';
-const categories = api + '/genres';
 const bestMovies = '?sort_by=-imdb_score,-votes'
+const category = '&genre=';
+const category1 = category + 'Sport';
+const category2 = category + 'Action';
+const category3 = category + 'Animation';
 const numberOfData = '&page_size='
 
 // Get sections from the DOM
 const app = document.querySelector('#app');
 const featuredMovie = document.querySelector('#featured-movie');
 const topRatedMovies = document.querySelector('#top-rated-movies');
-const category1 = document.querySelector('#category__1');
-const category2 = document.querySelector('#category__2');
-const category3 = document.querySelector('#category__3');
+const categorySection1 = document.querySelector('#category__1');
+const categorySection2 = document.querySelector('#category__2');
+const categorySection3 = document.querySelector('#category__3');
 
 
 /**
