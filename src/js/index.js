74d412d7fdbfd2a7e50312b3dfa09d7dd3ac560a
slv-improver <k.moret.slv@gmail.com>
2022-08-17 17:15:18 +0100

    define constants of DOM elements

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,12 @@ const api = 'http://127.0.0.1:8000/api/v1';
 const movies = api + '/titles';
 const categories = api + '/genres';
 const bestMovies = '?sort_by=-imdb_score,-votes'
+const numberOfData = '&page_size='
+
+// Get sections from the DOM
+const app = document.querySelector('#app');
+const featuredMovie = document.querySelector('#featured-movie');
+const topRatedMovies = document.querySelector('#top-rated-movies');
+const category1 = document.querySelector('#category__1');
+const category2 = document.querySelector('#category__2');
+const category3 = document.querySelector('#category__3');
